a1c65a9c51663a9bb0053dacf75f76e50bb86db0
Turchak411 <t-a-a-96@mail.ru>
2017-06-28 15:49:43 +0300

    Добавлено оформление меню

--- index.php
+++ index.php
@@ -1,2 +1,46 @@
 <?php
-//test
+?>
+
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Airline Terminal v0.0.1</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+
+    <div style="text-align:center">
+        <label><span class="label">
+                Welcome to Airline Terminal!
+                </span></label>
+    </div>
+</head>
+<body>
+<div class="img_center"><img src="resources/menu.png" width="270" height="283"></div>
+<div class="table_center">
+    <table class="trans_bc" border="1" width="200px">
+        <tr class="trans_bc">
+            <td align="center">
+                <form method="post" action="">
+                    <button type="submit" name="login">Sign up</button>
+                    <br>
+                </form>
+            </td>
+        </tr>
+        <tr class="trans_bc">
+            <td align="center">
+                <form method="post" action="">
+                    <button type="submit" name="registration">Registration</button>
+                    <br>
+                </form>
+            </td>
+        </tr>
+        <tr class="trans_bc">
+            <td align="center">
+                <form method="post" action="">
+                    <button type="submit" name="contacts">Contacts</button>
+                </form>
+            </td>
+        </tr>
+    </table>
+</div>
+</body>
+</html>
