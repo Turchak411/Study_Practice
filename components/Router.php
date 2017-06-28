8f596906a15dff7c7c6c9ef426c010b401a4c332
Pavel Ilyushchenko <pavel.ks1996@gmail.com>
2017-06-28 11:01:54 +0300

    Добавлен роутинг и настройки

---
+++ components/Router.php
@@ -0,0 +1,57 @@
+<?php
+
+class Router
+{
+
+    private $routes;
+
+    public function __construct()
+    {
+        $routesPath = ROOT . '/config/routes.php';
+        $this->routes = include($routesPath);
+    }
+
+    /**
+     * @return string строка запроса
+     */
+    private function getURI()
+    {
+        if (!empty($_SERVER['REQUEST_URI'])) {
+            return trim($_SERVER['REQUEST_URI'], '/');
+        }
+    }
+
+    /**
+     * Запускаем модуль Router на выполнение
+     */
+    public function run()
+    {
+        $uri = $this->getURI();
+
+        foreach ($this->routes as $uriPattern => $path) {
+
+            if (preg_match("~$uriPattern~", $uri)) {
+
+                $segments = explode('/', $path);
+
+                $controllerName = array_shift($segments) . 'Controller';
+                $controllerName = ucfirst($controllerName);
+
+
+                $actionName = 'action' . ucfirst((array_shift($segments)));
+
+                $controllerFile = ROOT . '/controllers/' . $controllerName . '.php';
+                if (file_exists($controllerFile)) {
+                    include_once($controllerFile);
+                }
+
+                $controllerObject = new $controllerName;
+                $result = $controllerObject->$actionName();
+                if ($result != null) {
+                    break;
+                }
+            }
+
+        }
+    }
+}
