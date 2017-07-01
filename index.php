2989ca4be6d650647282a8d39a5102f9309f1d49
Pavel Ilyushchenko <pavel.ks1996@gmail.com>
2017-07-01 15:45:40 +0300

    Добавдена автозагрузка классов, контроллеры и представления

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php
-//test
+
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+define('ROOT', dirname(__FILE__));
+require_once(ROOT.'/components/Autoload.php');
+
+$router = new Router();
+$router->run();
