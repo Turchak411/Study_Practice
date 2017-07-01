d8cdf414ff98ee4ef9bf289b5ebc308093c021c0
Pavel Ilyushchenko <pavel.ks1996@gmail.com>
2017-07-01 16:47:39 +0300

    Изменены представления

---
+++ views/main/index.php
@@ -0,0 +1,29 @@
+
+<div class="img_center"><img src="/template/resources/menu.png" width="270" height="283"></div>
+<div class="table_center">
+    <table class="trans_bc" border="1" width="200px">
+        <tr class="trans_bc">
+            <td align="center">
+                <form method="post" action="">
+                    <button type="submit" name="login">Sign up</button>
+                    <br>
+                </form>
+            </td>
+        </tr>
+        <tr class="trans_bc">
+            <td align="center">
+                <form method="post" action="">
+                    <button type="submit" name="registration">Registration</button>
+                    <br>
+                </form>
+            </td>
+        </tr>
+        <tr class="trans_bc">
+            <td align="center">
+                <form method="post" action="">
+                    <button type="submit" name="contacts">Contacts</button>
+                </form>
+            </td>
+        </tr>
+    </table>
+</div>
