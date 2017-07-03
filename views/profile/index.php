56017267f2693ecb593a586aab965e6d4da6c838
Turchak411 <t-a-a-96@mail.ru>
2017-07-03 15:32:36 +0300

    Редактирована корневая страница профиля

--- views/profile/index.php
+++ views/profile/index.php
@@ -6,14 +6,13 @@
         <meta name="viewport"
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Авторизация</title>
+        <title>Профиль</title>
         <link rel="stylesheet" href="/template/css/bootstrap.css">
         <link rel="stylesheet" href="/template/css/bootstrap-theme.css">
         <link rel="stylesheet" href="/template/css/style_profile.css">
         <h1><?= $login?>>
             <small>Profile</small>
         </h1>
-        <br<br<br><br>
     </head>
     <body>
     <div class="container">
@@ -21,7 +20,7 @@
             <div class="form-group">
                 <label for="login" class="col-sm-5 control-label text-center">Login</label>
                 <div class="col-sm-3">
-                    <input type="login" name="login" class="form-control" id="login" placeholder="Login"
+                    <input type="text" name="login" class="form-control" id="login" placeholder="Login"
                            value="<?= $login?>">
                 </div>
             </div>
@@ -35,7 +34,7 @@
             <div class="form-group">
                 <label for="password" class="col-sm-5 control-label text-center">Password</label>
                 <div class="col-sm-3">
-                    <input type="text" name="login" class="form-control" id="login" placeholder="Password"
+                    <input type="text" name="password" class="form-control" id="password" placeholder="Password"
                            value="<?= $password?>">
                 </div>
             </div>
