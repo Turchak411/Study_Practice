7089864569ee02e4296a3d073755faef65ecefce
Илющенко Павел <pavel.ks1996@gmail.com>
2017-07-06 01:30:27 +0300

    Добавлен профиль пользователя с контролем доступа

--- controllers/ProfileController.php
+++ controllers/ProfileController.php
@@ -1,15 +1,29 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: Pavel
- * Date: 01.07.17
- * Time: 18:08
- */
 class ProfileController extends BaseController
 {
+    function __construct()
+    {
+        Profile::checkPrivateAccess();
+    }
+
     public function actionIndex()
     {
+        if (Profile::isNeedEditAirlineFrofile(1))
+        {
+            header("Location: /profile/edit");
+            return true;
+        }
+        if (!Profile::isUserValidated(1))
+        {
+            return self::Render('profile', 'waitingForConfirmation');
+        }
+
         return self::Render('profile', 'index');
+    }
+
+    public function actionEdit()
+    {
+        return self::Render('profile', 'edit');
     }
 }
