d9b126a822aba959d38a0eb0603e49483436d428
Илющенко Павел <pavel.ks1996@gmail.com>
2017-07-05 21:50:49 +0300

    Изменены пути

--- config/routes.php
+++ config/routes.php
@@ -5,11 +5,16 @@
  * 'контроллер' => 'контроллер/метод',
  */
 return array(
+    'admin/request/([0-9]+)' => 'admin/request/$1',
+    'admin/requests' => 'admin/requestList',
+    'admin' => 'admin/index',
     'user/registration' => 'user/registration',
     'user/logout' => 'user/logout',
     'user/login' => 'user/login',
+    'airlines' => 'airline/index',
+    'airline/([0-9]+)' => 'airline/show/$1',
     'setup' => 'setup/index',
     'airplane' => 'airplane/index',
     'main/main' => 'main/main',
-    ''=>'main/index'
+    '' => 'main/index'
 );
