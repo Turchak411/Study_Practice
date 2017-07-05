ccbe94b3492bb71638671d66b2fce2de01d1b72b
Илющенко Павел <pavel.ks1996@gmail.com>
2017-07-05 21:27:35 +0300

    Добавлена передача параметров в методы

--- components/Router.php
+++ components/Router.php
@@ -40,13 +40,21 @@ class Router
 
                 $actionName = 'action' . ucfirst((array_shift($segments)));
 
+                $parameters = $segments;
+
                 $controllerFile = ROOT . '/controllers/' . $controllerName . '.php';
+
                 if (file_exists($controllerFile)) {
                     include_once($controllerFile);
                 }
 
                 $controllerObject = new $controllerName;
-                $result = $controllerObject->$actionName();
+                /*
+                 * Вызываем необходимый метод ($actionName) у определенного
+                 * класса ($controllerObject) с заданными ($parameters) параметрами
+                 */
+                $result = call_user_func_array(array($controllerObject, $actionName), $parameters);
+
                 if ($result != null) {
                     break;
                 }
